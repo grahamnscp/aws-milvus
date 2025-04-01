8342ea9360022d21e5f6430bf3895b4bf9755fce
grahamnscp <grahamnscp@gmail.com>
2025-04-01 15:44:00 +0100

    test milvus values

--- 03-install-milvus.sh
+++ 03-install-milvus.sh
@@ -12,18 +12,18 @@ helm registry login dp.apps.rancher.io/charts -u $APPCOL_USER -p $APPCOL_TOKEN
 
 # ----------------------------
 # install cert manager 
-Log "\_Creating cert-manager namespace.."
-kubectl --kubeconfig=./local/admin.conf create namespace cert-manager
+#Log "\_Creating cert-manager namespace.."
+#kubectl --kubeconfig=./local/admin.conf create namespace cert-manager
 
-Log "\_Creating application-collection secret for cert-manager.."
-kubectl --kubeconfig=./local/admin.conf create secret docker-registry application-collection --docker-server=dp.apps.rancher.io --docker-username=$APPCOL_USER --docker-password=$APPCOL_TOKEN -n cert-manager
+#Log "\_Creating application-collection secret for cert-manager.."
+#kubectl --kubeconfig=./local/admin.conf create secret docker-registry application-collection --docker-server=dp.apps.rancher.io --docker-username=$APPCOL_USER --docker-password=$APPCOL_TOKEN -n cert-manager
 
-Log "\_Installing cert-manager.."
-helm upgrade --kubeconfig=./local/admin.conf --install cert-manager \
-  oci://dp.apps.rancher.io/charts/cert-manager \
-  -n cert-manager \
-  --set crds.enabled=true \
-  --set 'global.imagePullSecrets[0].name'=application-collection
+#Log "\_Installing cert-manager.."
+#helm upgrade --kubeconfig=./local/admin.conf --install cert-manager \
+#  oci://dp.apps.rancher.io/charts/cert-manager \
+#  -n cert-manager \
+#  --set crds.enabled=true \
+#  --set 'global.imagePullSecrets[0].name'=application-collection
 
 # ----------------------------
 Log "\_Creating suse-ai namespace.."
@@ -47,16 +47,20 @@ global:
 cluster:
   enabled: false
 standalone:
+  messageQueue: rocksmq
   persistence:
+    mountPath: "/var/lib/milvus"
+    enabled: true
     persistentVolumeClaim:
       storageClass: longhorn
+      size: 20Gi
 etcd:
   replicaCount: 1
   persistence:
     storageClassName: longhorn
 minio:
   mode: distributed
-  replicas: 2
+  replicas: 4
   rootUser: "admin"
   rootPassword: "adminminio"
   persistence:
@@ -66,8 +70,8 @@ minio:
     requests:
       memory: 1024Mi
 kafka:
-  enabled: false
   name: kafka
+  enabled: false
 MV1EOF
 
 cat << MVEOF >./local/milvus-values.yaml
@@ -77,26 +81,37 @@ global:
 cluster:
   enabled: true
 standalone:
+  messageQueue: kafka
   persistence:
+    enabled: true
+    mountPath: "/var/lib/milvus"
     persistentVolumeClaim:
       storageClass: longhorn
+      size: 20Gi
 etcd:
-  replicaCount: 1
+  replicaCount: 3
   persistence:
     storageClassName: longhorn
 minio:
   mode: distributed
-  replicas: 2
+  replicas: 4
   rootUser: "admin"
   rootPassword: "adminminio"
   persistence:
     storageClass: longhorn
-    size: 30Gi
+    size: 10Gi
   resources:
     requests:
       memory: 1024Mi
 kafka:
+  name: kafka
+  enabled: true
   persistence:
+    accessModes:
+      - ReadWriteOnce
+    resources:
+      requests:
+        storage: 8Gi
     storageClassName: longhorn
 MVEOF
 
@@ -104,8 +119,9 @@ Log " \_Installing milvus database.."
 helm upgrade --kubeconfig=./local/admin.conf \
   --install milvus oci://dp.apps.rancher.io/charts/milvus \
   -n suse-ai \
-  -f ./local/milvus-values.yaml \
-  --timeout=10m
+  -f ./local/milvus-values-nokafka.yaml \
+  --set ignore-formatted=true \
+  --timeout=5m
 
 # --------------------------------------------------------------------
 
